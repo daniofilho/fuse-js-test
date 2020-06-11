ee798f819fcc7b76e58a08163ce03056906fce89
Dânio <danio.filho@navegarti.com.br>
2020-06-11 14:43:03 -0300

    first test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 const Fuse = require('fuse.js');
-
 const data = require('./data.js');
 
-console.log('Setup OK!');
+const defaultOptions = {};
+
+// # Setup
+const searchByTagName = new Fuse(data, {
+  defaultOptions,
+  keys: ['tags.Nome'],
+});
+
+// # Searches
+
+// ## By Tag Name
+function fn_searchByTagName() {
+  const search = 'SEGMENTO';
+  const result = searchByTagName.search(search);
+  console.log(`
+    Searching by Tag Name: ${search}
+    Result:
+  `);
+  console.log(result);
+}
+
+// # Outputs
+fn_searchByTagName();
